4f96bb38866debbabdba6ba174633c47783385b7
fraziercs2i <jgwenet@hotmail.com>
2021-10-14 09:36:26 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'; // lifting the state up 
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+// we can acess username and age here in App because
+//it is among Userslist and AddUsers
+//and we use the state lifting up to get those attributes here.
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name:uName, age: uAge, id: Math.random().toString()}];
+    });
+  };
+
+  return (
+    <div>
+        <AddUser onAddUser = {addUserHandler} />
+        <UsersList users = {usersList}/>
+    </div>
+  );
+};
+
+export default App;
